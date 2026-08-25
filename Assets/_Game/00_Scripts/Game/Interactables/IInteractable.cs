3832cace9590ecb98fc0f9056dbfbac155e22edd
Muhammad Zaini <muhammadzaini1118@gmail.com>
2026-08-25 08:18:02 +0800

    Feat/UI/hud (#55)
    * feat: player interactable prompt HUD
    
    * feat: prefabbing

--- Assets/_Game/00_Scripts/Game/Interactables/IInteractable.cs
+++ Assets/_Game/00_Scripts/Game/Interactables/IInteractable.cs
@@ -1,13 +1,9 @@
 namespace Slafurry.Player
 {
-    /// <summary>
-    /// Implement this on any object the player should be able to interact
-    /// with via the Interact input (E). Attach the implementing component
-    /// to a GameObject with a Collider2D on the interactable layer so
-    /// PlayerInteract can detect it.
-    /// </summary>
     public interface IInteractable
     {
+        string Prompt { get; }
+
         void Interact();
     }
 }
--- Assets/_Game/00_Scripts/Game/Triggers/InteractionTrigger.cs
+++ Assets/_Game/00_Scripts/Game/Triggers/InteractionTrigger.cs
@@ -7,7 +7,10 @@ namespace Slafurry.Interaction
     public class InteractionTrigger : MonoBehaviour, IInteractable
     {
         [Header("Interaction")]
+        [SerializeField] private string prompt = "Interact";
         [SerializeField] private UnityEvent onInteract;
+
+        public string Prompt => prompt;
 
         public void Interact()
         {
